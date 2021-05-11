031ce06cf0b83e01db0d29c7dfd9086b26fc07f8
Haneen Amro <heno.amro@gmail.com>
2021-05-11 09:46:03 -0700

    Added all propetires of data JSON as a props from parent (CarList) to the child (singleCar)

--- cars/src/carList.js
+++ cars/src/carList.js
@@ -4,6 +4,13 @@ const CarList = (props) => {
 
     return (
         <div>
+            { props.carData.map((item) => (
+                < SingleCar make={item.make} year={item.year} model={item.model} vin={item.vin} color={item.color} value={props.value}
+                    price={item.askingPrice} cost={item.dealerCost} dateListed={item.dateListed} lotName={item.lot.name} 
+                    lotCity={item.lot.city}  lotCountry={item.lot.country}
+                />
+            )) 
+            }
             
         </div>
     )
