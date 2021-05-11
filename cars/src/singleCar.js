252ecd6010892502242b1bd917b5c99a449a7617
Haneen Amro <heno.amro@gmail.com>
2021-05-11 10:15:59 -0700

    Used Accordion-like componet

--- cars/src/singleCar.js
+++ cars/src/singleCar.js
@@ -1,18 +1,34 @@
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+
 const SingleCar = (props) => {
     return (
         <div className="singleCar">
-            <div> Year: {props.year} </div>            
-            <div> Make: {props.make} </div>
-            <div> Model: {props.model} </div> 
-            <div> Price: {props.price}</div>
-            <div> color: {props.color} </div>
-            <div> vin: {props.vin} </div>
-            <div>Dealer cost: {props.coast}</div>
-            <div> Lot :
-                <span> Name: {props.lotName}</span>
-                <span> /City: {props.lotCity}</span>
-                <span> /Country: {props.lotCountry}</span>
-            </div>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>
+                        <div> Year: {props.year} </div>            
+                        <div> Make: {props.make} </div>
+                        <div> Model: {props.model} </div> 
+                        <div> Price: {props.price}</div>
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <div> color: {props.color} </div>
+                        <div> vin: {props.vin} </div>
+                        <div>Dealer cost: {props.coast}</div>
+                        <div> Lot :
+                            <span> Name: {props.lotName}</span>
+                            <span> /City: {props.lotCity}</span>
+                            <span> /Country: {props.lotCountry}</span>
+                        </div>
+
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </div>
     )
 }
