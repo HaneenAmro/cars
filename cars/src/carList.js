c3bb846f967da5d90935d9bf748c42efb7d741dc
Haneen Amro <heno.amro@gmail.com>
2021-05-11 11:45:44 -0700

    search by all the properties instead the numbers

--- cars/src/carList.js
+++ cars/src/carList.js
@@ -13,18 +13,17 @@ const CarList = (props) => {
                             lotCity={item.lot.city}  lotCountry={item.lot.country}
                         />
                     )) 
-
                 :
-                    props.carData.filter((item) => (
-                        Object.values(item).includes(props.value) || Object.values(item.lot).includes(props.value)
-                    )).map((item) => (
+                    props.carData.filter((item) => {
+                        let upperValue = props.value.charAt(0).toUpperCase()+ props.value.slice(1);
+                        return Object.values(item).includes(upperValue) || Object.values(item.lot).includes(upperValue)
+                    }).map((item) => (
                         < SingleCar make={item.make} year={item.year} model={item.model} vin={item.vin} color={item.color} value={props.value}
                             price={item.askingPrice} cost={item.dealerCost} dateListed={item.dateListed} lotName={item.lot.name} 
                             lotCity={item.lot.city}  lotCountry={item.lot.country}
                         />
                     )) 
-            }
-            
+            }     
         </div>
     )
 
