659b1ccea8b427d2174f5dcc9658b1f8c34e29dd
Haneen Amro <heno.amro@gmail.com>
2021-05-11 09:57:54 -0700

    Rendered a list of all the cars properties

--- cars/src/singleCar.js
+++ cars/src/singleCar.js
@@ -1,7 +1,18 @@
 const SingleCar = (props) => {
     return (
         <div>
-            
+            <div> Year: {props.year} </div>            
+            <div> Make: {props.make} </div>
+            <div> Model: {props.model} </div> 
+            <div> Price: {props.price}</div>
+            <div> color: {props.color} </div>
+            <div> vin: {props.vin} </div>
+            <div>Dealer cost: {props.coast}</div>
+            <div> Lot :
+                <span> Name: {props.lotName}</span>
+                <span> /City: {props.lotCity}</span>
+                <span> /Country: {props.lotCountry}</span>
+            </div>
         </div>
     )
 }
