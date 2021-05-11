76b162234b808c572d43cdbb437fdccd41d0613c
Haneen Amro <heno.amro@gmail.com>
2021-05-11 09:26:27 -0700

    Refactored App.js to be statfull function and rendered list of the cars make

--- cars/src/App.js
+++ cars/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import { carData } from './carData';
+import React from 'react'; 
 import './App.css';
 
-function App() {
+class App extends React.Component  {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      carData : carData,
+    }
+  }
+
+  render() {
   return (
     <div className="App">
+      {this.state.carData.map((item) => (
+        <div>{item.make}</div>
+      ))}
     </div>
   );
+  }
 }
 
 export default App;
